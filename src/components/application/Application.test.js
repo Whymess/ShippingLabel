18c241e5a3bf2b31bbae56272ce5e3259f37a0ca
Yoni Messing <Jonathan.lev.messing@gmail.com>
2021-03-07 16:34:27 -0500

    find text

--- src/components/application/Application.test.js
+++ src/components/application/Application.test.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
-
 import { Router } from "react-router-dom";
 
 import Application from "./index";
 
-describe("App", () => {
-  test("renders App component", () => {
+describe("Application", () => {
+  test("should render application component", () => {
     const history = createMemoryHistory();
     render(
       <Router history={history}>
         <Application />
       </Router>
     );
+    expect(screen.getByText("Shipping label Maker")).toBeInTheDocument();
   });
 });
